d37545c1cdaeb0ab93c86a9d9dc7a93caf95471d
Endless Hypnosis <endlesshypnosis@gmail.com>
2017-09-18 01:08:03 -0600

    updating package json and testing

--- src/test/test_helper.js
+++ src/test/test_helper.js
@@ -1,20 +1,30 @@
+//
+// "test": "./node_modules/.bin/mocha --compilers js:babel-core/register --require ./test/test_helper.js --recursive ./test",
+//
+
 import _$ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+const { JSDOM } = jsdom;
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '../src/app/reducers';
+import reducers from '../app/reducers/index';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
+const { document } = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+global.document = document;
+global.window = document.defaultView;
+
+
+// global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+// global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 const $ = _$(window);
 
-chaiJquery(chai, chai.util, $);
+// chaiJquery(chai, chai.util, $);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
   const componentInstance =  TestUtils.renderIntoDocument(
@@ -26,11 +36,11 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-};
+// $.fn.simulate = function(eventName, value) {
+//   if (value) {
+//     this.val(value);
+//   }
+//   TestUtils.Simulate[eventName](this[0]);
+// };
 
 export {renderComponent, expect};
